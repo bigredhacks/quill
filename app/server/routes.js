6e6cf28687ea71f69c3f14fec47641380a186864
Max Chu <maxmchu@gmail.com>
2019-03-15 10:04:33 -0400

    LRH event redirect

--- app/server/routes.js
+++ app/server/routes.js
@@ -3,6 +3,11 @@ const path = require("path");
 const indexLocation = path.resolve(__dirname, "../client/index.html");
 
 module.exports = function (app) {
+
+    app.get("/little", (req, res) => {
+        res.redirect("https://littleredhacks.typeform.com/to/VYi7s0");
+    })
+
     // Application ------------------------------------------
     app.get("/", (req, res) => {
         res.sendFile(indexLocation);
